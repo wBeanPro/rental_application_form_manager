54d0cadc0a3ef8195ad974fb5d90e9700d86e927
woobinpro <woobinpro@gmail.com>
2023-03-24 09:38:46 +0900

    rental application form - manager

---
+++ README.md
@@ -0,0 +1,18 @@
+# simple-node-bitcoin-wallet
+Simple Nodejs App to manage your bitcoin wallet
+
+A light and simple app for managing your Bitcoin wallet.
+
+After starting bitcoin full node, install all nodejs modules with <code>npm install </code> then type <code>node index.js</code> to start app.
+
+navigate to app by typing:
+
+<code>YOUR_SERVER_IP:3008</code>
+
+Default password is:
+
+<code>123456</code>
+
+You can change password from index.js file.
+
+done.
---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var bodyParser = require("body-parser");
+var expressSession = require('express-session');
+var authentication = require('./authentication');
+var port = 80;
+
+const PASSWORD = 'domisol1'; // Replace your password
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Session middleware
+const session = expressSession({
+	secret: 'secret',
+	resave: false,
+	saveUninitialized: false,
+	cookie: { maxAge: 1 * 24 * 60 * 60 * 1000 } // 1 day
+});
+app.use(session);
+
+// Auth middleware
+app.use(authentication);
+
+app.get('/', function(req, res){
+	if (req.authenticated) {
+		res.render('index', { date: new Date()});
+	} else {
+		res.render('login');
+	}
+});
+
+
+app.post('/login', function(req, res){
+	const { password } = req.body;
+	if(password === PASSWORD){
+		req.login(true);
+		res.redirect('/');
+	}
+});
+
+server.listen(port, function(){
+	console.log('listening on *:' + port);
+});
